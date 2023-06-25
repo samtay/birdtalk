ca6f3a1f1f2cccac5e7ab2e9bb843431176778ec
Sam Tay <samctay@pm.me>
2023-06-25 15:02:44 -0600

    Port game template to birdtalk

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+// Copyright 2022, the Flutter project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 // Uncomment the following lines when enabling Firebase Crashlytics
 // import 'dart:io';
 // import 'package:firebase_core/firebase_core.dart';
@@ -5,12 +9,33 @@
 
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:go_router/go_router.dart';
+import 'package:logging/logging.dart';
 import 'package:provider/provider.dart';
 
+import 'src/ads/ads_controller.dart';
 import 'src/app_lifecycle/app_lifecycle.dart';
+import 'src/audio/audio_controller.dart';
 import 'src/crashlytics/crashlytics.dart';
+import 'src/games_services/games_services.dart';
+import 'src/games_services/score.dart';
+import 'src/in_app_purchase/in_app_purchase.dart';
+import 'src/level_selection/level_selection_screen.dart';
+import 'src/level_selection/levels.dart';
+import 'src/main_menu/main_menu_screen.dart';
+import 'src/play_session/play_session_screen.dart';
+import 'src/player_progress/persistence/local_storage_player_progress_persistence.dart';
+import 'src/player_progress/persistence/player_progress_persistence.dart';
+import 'src/player_progress/player_progress.dart';
+import 'src/settings/persistence/local_storage_settings_persistence.dart';
+import 'src/settings/persistence/settings_persistence.dart';
+import 'src/settings/settings.dart';
+import 'src/settings/settings_screen.dart';
+import 'src/style/my_transition.dart';
 import 'src/style/palette.dart';
+import 'src/style/snack_bar.dart';
+import 'src/win_game/win_game_screen.dart';
 
 Future<void> main() async {
   // To enable Firebase Crashlytics, uncomment the following lines and
@@ -36,24 +61,146 @@ Future<void> main() async {
   );
 }
 
+/// Without logging and crash reporting, this would be `void main()`.
 void guardedMain() {
-  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+
+  _log.info('Going full screen');
+  SystemChrome.setEnabledSystemUIMode(
+    SystemUiMode.edgeToEdge,
+  );
+
+  // TODO: When ready, uncomment the following lines to enable integrations.
+  //       Read the README for more info on each integration.
+
+  AdsController? adsController;
+  // if (!kIsWeb && (Platform.isIOS || Platform.isAndroid)) {
+  //   /// Prepare the google_mobile_ads plugin so that the first ad loads
+  //   /// faster. This can be done later or with a delay if startup
+  //   /// experience suffers.
+  //   adsController = AdsController(MobileAds.instance);
+  //   adsController.initialize();
+  // }
+
+  GamesServicesController? gamesServicesController;
+  // if (!kIsWeb && (Platform.isIOS || Platform.isAndroid)) {
+  //   gamesServicesController = GamesServicesController()
+  //     // Attempt to log the player in.
+  //     ..initialize();
+  // }
+
+  InAppPurchaseController? inAppPurchaseController;
+  // if (!kIsWeb && (Platform.isIOS || Platform.isAndroid)) {
+  //   inAppPurchaseController = InAppPurchaseController(InAppPurchase.instance)
+  //     // Subscribing to [InAppPurchase.instance.purchaseStream] as soon
+  //     // as possible in order not to miss any updates.
+  //     ..subscribe();
+  //   // Ask the store what the player has bought already.
+  //   inAppPurchaseController.restorePurchases();
+  // }
+
+  runApp(
+    MyApp(
+      settingsPersistence: LocalStorageSettingsPersistence(),
+      playerProgressPersistence: LocalStoragePlayerProgressPersistence(),
+      inAppPurchaseController: inAppPurchaseController,
+      adsController: adsController,
+      gamesServicesController: gamesServicesController,
+    ),
+  );
 }
 
-//Logger _log = Logger('main.dart');
+Logger _log = Logger('main.dart');
 
 class MyApp extends StatelessWidget {
   static final _router = GoRouter(
     routes: [
       GoRoute(
           path: '/',
-          builder: (context, state) => MyHomePage(),
-          routes: const []
-      ),
-    ]
+          builder: (context, state) =>
+              const MainMenuScreen(key: Key('main menu')),
+          routes: [
+            GoRoute(
+                path: 'play',
+                pageBuilder: (context, state) => buildMyTransition<void>(
+                      key: ValueKey('play'),
+                      child: const LevelSelectionScreen(
+                        key: Key('level selection'),
+                      ),
+                      color: context.watch<Palette>().backgroundLevelSelection,
+                    ),
+                routes: [
+                  GoRoute(
+                    path: 'session/:level',
+                    pageBuilder: (context, state) {
+                      final levelNumber =
+                          int.parse(state.pathParameters['level']!);
+                      final level = gameLevels
+                          .singleWhere((e) => e.number == levelNumber);
+                      return buildMyTransition<void>(
+                        key: ValueKey('level'),
+                        child: PlaySessionScreen(
+                          level,
+                          key: const Key('play session'),
+                        ),
+                        color: context.watch<Palette>().backgroundPlaySession,
+                      );
+                    },
+                  ),
+                  GoRoute(
+                    path: 'won',
+                    redirect: (context, state) {
+                      if (state.extra == null) {
+                        // Trying to navigate to a win screen without any data.
+                        // Possibly by using the browser's back button.
+                        return '/';
+                      }
+
+                      // Otherwise, do not redirect.
+                      return null;
+                    },
+                    pageBuilder: (context, state) {
+                      final map = state.extra! as Map<String, dynamic>;
+                      final score = map['score'] as Score;
+
+                      return buildMyTransition<void>(
+                        key: ValueKey('won'),
+                        child: WinGameScreen(
+                          score: score,
+                          key: const Key('win game'),
+                        ),
+                        color: context.watch<Palette>().backgroundPlaySession,
+                      );
+                    },
+                  )
+                ]),
+            GoRoute(
+              path: 'settings',
+              builder: (context, state) =>
+                  const SettingsScreen(key: Key('settings')),
+            ),
+          ]),
+    ],
   );
 
-  const MyApp({super.key});
+  final PlayerProgressPersistence playerProgressPersistence;
+
+  final SettingsPersistence settingsPersistence;
+
+  final GamesServicesController? gamesServicesController;
+
+  final InAppPurchaseController? inAppPurchaseController;
+
+  final AdsController? adsController;
+
+  const MyApp({
+    required this.playerProgressPersistence,
+    required this.settingsPersistence,
+    required this.inAppPurchaseController,
+    required this.adsController,
+    required this.gamesServicesController,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -61,219 +208,65 @@ class MyApp extends StatelessWidget {
       child: MultiProvider(
         providers: [
           ChangeNotifierProvider(
-            create: (context) => MyAppState(),
+            create: (context) {
+              var progress = PlayerProgress(playerProgressPersistence);
+              progress.getLatestFromStore();
+              return progress;
+            },
+          ),
+          Provider<GamesServicesController?>.value(
+              value: gamesServicesController),
+          Provider<AdsController?>.value(value: adsController),
+          ChangeNotifierProvider<InAppPurchaseController?>.value(
+              value: inAppPurchaseController),
+          Provider<SettingsController>(
+            lazy: false,
+            create: (context) => SettingsController(
+              persistence: settingsPersistence,
+            )..loadStateFromPersistence(),
+          ),
+          ProxyProvider2<SettingsController, ValueNotifier<AppLifecycleState>,
+              AudioController>(
+            // Ensures that the AudioController is created on startup,
+            // and not "only when it's needed", as is default behavior.
+            // This way, music starts immediately.
+            lazy: false,
+            create: (context) => AudioController()..initialize(),
+            update: (context, settings, lifecycleNotifier, audio) {
+              if (audio == null) throw ArgumentError.notNull();
+              audio.attachSettings(settings);
+              audio.attachLifecycleNotifier(lifecycleNotifier);
+              return audio;
+            },
+            dispose: (context, audio) => audio.dispose(),
           ),
           Provider(
             create: (context) => Palette(),
-          )
+          ),
         ],
         child: Builder(builder: (context) {
           final palette = context.watch<Palette>();
 
-          // TODO why isn't my background yellow... ?
           return MaterialApp.router(
-            title: 'BirdTalk',
+            title: 'Flutter Demo',
             theme: ThemeData.from(
-              useMaterial3: true,
               colorScheme: ColorScheme.fromSeed(
-                  seedColor: palette.darkPen,
-                  background: palette.backgroundMain,
+                seedColor: palette.darkPen,
+                background: palette.backgroundMain,
               ),
               textTheme: TextTheme(
                 bodyMedium: TextStyle(
                   color: palette.ink,
-                  fontFamily: 'Permanent Marker'
                 ),
               ),
+              useMaterial3: true,
             ),
             routeInformationProvider: _router.routeInformationProvider,
             routeInformationParser: _router.routeInformationParser,
             routerDelegate: _router.routerDelegate,
+            scaffoldMessengerKey: scaffoldMessengerKey,
           );
         }),
-      )
-    );
-  }
-}
-
-class MyAppState extends ChangeNotifier {
-  var current = "BirdTalk";
-  void getNext() {
-    current = "Talk Birdy To Me";
-    notifyListeners();
-  }
-
-  Set<String> favorites = {};
-
-  void toggleFavorite() {
-    if (favorites.contains(current)) {
-      favorites.remove(current);
-    } else {
-      favorites.add(current);
-    }
-    notifyListeners();
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  var selectedIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-
-    Widget page;
-    switch (selectedIndex) {
-      case 0:
-        page = GeneratorPage();
-        break;
-      case 1:
-        page = FavoritesPage();
-        break;
-      default:
-        throw UnimplementedError('no widget for $selectedIndex');
-    }
-
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        return Scaffold(
-            body: Row(
-                children: [
-                  SafeArea(
-                      child: NavigationRail(
-                        extended: constraints.maxWidth >= 600,
-                        destinations: [
-                          NavigationRailDestination(
-                            icon: Icon(Icons.home),
-                            label: Text('Home'),
-                          ),
-                          NavigationRailDestination(
-                            icon: Icon(Icons.favorite),
-                            label: Text('Favorites'),
-                          )
-                        ],
-                        selectedIndex: selectedIndex,
-                        onDestinationSelected: (value) {
-                          setState(() {
-                            selectedIndex = value;
-                          });
-                        },
-                    ),
-                  ),
-                  Expanded(
-                      child: Container(
-                          color: Theme.of(context).colorScheme.primaryContainer,
-                          child: page,
-                      ),
-                  ),
-                ],
-              ),
-            );
-      }
-    );
-  }
-}
-
-class GeneratorPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var pair = appState.current;
-    var favorited = appState.favorites.contains(pair);
-
-    return Center(
-      child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        BigCard(pair: pair),
-        SizedBox(height: 15),
-        Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            ElevatedButton.icon(
-              onPressed: () {
-                appState.toggleFavorite();
-              },
-              icon: Icon(favorited ? Icons.favorite : Icons.favorite_outline),
-              label: Text('Like'),
-            ),
-            SizedBox(width: 15),
-            ElevatedButton(
-              onPressed: () {
-                appState.getNext();
-              },
-              child: Text('Next'),
-            ),
-          ],
-        ),
-      ],
-    ));
-  }
-}
-
-class BigCard extends StatelessWidget {
-  const BigCard({
-    super.key,
-    required this.pair,
-  });
-
-  final String pair;
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-
-    final style = theme.textTheme.displayMedium!.copyWith(
-      color: theme.colorScheme.onPrimary,
-      letterSpacing: 1.0,
-      // TODO figure out how to propagate this font through theme
-      fontFamily: 'Permanent Marker',
-      fontSize: 50
-    );
-    return Card(
-      color: theme.colorScheme.tertiary,
-      elevation: 5.0,
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Text(
-          pair,
-          style: style,
-          semanticsLabel: "${pair} ${pair}",
-        ),
-      ),
-    );
-  }
-}
-
-class FavoritesPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var favorites = appState.favorites;
-
-    if (favorites.isEmpty) {
-      return Center(
-          child: Text('None yet!')
-      );
-    }
-
-    return Center(
-      child: ListView(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: Text('You have ${favorites.length} favorites:'),
-          ),
-          for (var pair in favorites)
-            ListTile(
-                leading: Icon(Icons.favorite),
-                title: Text(pair),
-            ),
-        ]
       ),
     );
   }
