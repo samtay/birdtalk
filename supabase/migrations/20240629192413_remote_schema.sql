1d55014e942a2b9ded7e51b6a577db22f87f0f03
Sam Tay <samtay@mercury.com>
2026-07-09 19:19:25 -0400

    Fix local supabase

--- supabase/migrations/20240629192413_remote_schema.sql
+++ supabase/migrations/20240629192413_remote_schema.sql
@@ -10,8 +10,6 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
-CREATE EXTENSION IF NOT EXISTS "pgsodium" WITH SCHEMA "pgsodium";
-
 COMMENT ON SCHEMA "public" IS 'standard public schema';
 
 CREATE EXTENSION IF NOT EXISTS "pg_graphql" WITH SCHEMA "graphql";
